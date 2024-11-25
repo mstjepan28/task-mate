7f7ae9e947b44317445d8bc54956cc49ce53e960
mstjepan28 <markovcics@gmail.com>
2024-11-25 19:51:11 +0100

    task form header ui fixes

--- src/components/form/TaskForm.tsx
+++ src/components/form/TaskForm.tsx
@@ -10,12 +10,12 @@ interface IProps {
 export const TaskForm = ({ edit }: IProps) => {
   return (
     <div className="flex min-h-full flex-col">
-      <div className="flex items-center gap-x-2 p-2">
+      <div className="flex items-center gap-x-2 border-b border-gray-900 px-2 py-3">
         <Link href="/">
           <HiChevronLeft className="" size={24} />
         </Link>
 
-        <span>{edit ? "Edit task" : "Create task"}</span>
+        <span className="text-lg font-semibold">{edit ? "Edit task" : "Create task"}</span>
       </div>
 
       <div className="basis-full"></div>
