7bd4ac26f30aea24746dd9c602739a8aa0007284
mstjepan28 <markovcics@gmail.com>
2024-11-25 19:31:11 +0100

    added navigation functionality to bottom nav bar

--- src/components/layout/BottomNav.tsx
+++ src/components/layout/BottomNav.tsx
@@ -1,15 +1,20 @@
+import Link from "next/link";
 import { HiMenu, HiOutlineClipboardList } from "react-icons/hi";
 
 export const BottomNav = () => {
   return (
     <div className="grid grid-cols-5 gap-x-2 border-t border-t-gray-900 p-2">
-      <BottomNavItem>
+      <BottomNavItem to="/">
         <HiOutlineClipboardList size={24} />
       </BottomNavItem>
+
       <BottomNavItem>btn</BottomNavItem>
+
       <BottomNavItem>btn</BottomNavItem>
+
       <BottomNavItem>btn</BottomNavItem>
-      <BottomNavItem>
+
+      <BottomNavItem to="/profile">
         <HiMenu size={24} />
       </BottomNavItem>
     </div>
@@ -17,8 +22,13 @@ export const BottomNav = () => {
 };
 
 interface IBottomNavItemProps {
+  to?: string;
   children: React.ReactNode;
 }
-const BottomNavItem = ({ children }: IBottomNavItemProps) => {
-  return <div className="flex items-center justify-center py-2">{children}</div>;
+const BottomNavItem = ({ to, children }: IBottomNavItemProps) => {
+  return (
+    <Link href={to ?? "/"} className="flex items-center justify-center py-2">
+      {children}
+    </Link>
+  );
 };
