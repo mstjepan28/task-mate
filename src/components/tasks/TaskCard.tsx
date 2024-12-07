43f5db6a1099e45972fb6d6ec326babfed9ce82e
mstjepan28 <markovcics@gmail.com>
2024-12-07 18:32:05 +0100

    simplified task card component style

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -1,36 +1,29 @@
 import type { Task } from "@prisma/client";
 import dayjs from "dayjs";
 import Link from "next/link";
+import { twMerge } from "tailwind-merge";
 import { TaskStatus } from "~/enums/taskStatus";
 
 export const TaskCard = ({ task }: { task: Task }) => {
-  const card = {
-    [TaskStatus.PENDING]: "bg-yellow-400/90 border-yellow-700",
-    [TaskStatus.STARTED]: "bg-blue-600/75 border-blue-700",
-    [TaskStatus.CANCELED]: "bg-orange-500/75 border-orange-600",
-    [TaskStatus.DONE]: "bg-green-600/75 border-green-700",
-    [TaskStatus.FAILED]: "bg-red-600/75 border-red-700",
-  }[task.status];
-
-  const text = {
-    [TaskStatus.PENDING]: "text-yellow-800",
-    [TaskStatus.STARTED]: "text-blue-800",
-    [TaskStatus.CANCELED]: "text-orange-800",
-    [TaskStatus.DONE]: "text-green-800",
-    [TaskStatus.FAILED]: "text-red-800",
+  const statusStyle = {
+    [TaskStatus.STARTED]: "bg-blue-100 text-blue-800",
+    [TaskStatus.PENDING]: "bg-yellow-100 text-yellow-800",
+    [TaskStatus.FAILED]: "bg-red-100 text-red-800",
+    [TaskStatus.DONE]: "bg-green-100 text-green-800",
+    [TaskStatus.CANCELED]: "bg-gray-100 text-gray-800",
   }[task.status];
 
   return (
     <Link
       href={`/task/edit/${task.id}`}
-      className={`w-full rounded-lg border-2 px-2 py-2 shadow-sm sm:min-w-0 ${card}`}
+      className="w-full rounded-lg border border-primary bg-white p-2 text-primary sm:min-w-0"
     >
-      <div className="flex justify-between text-sm">
-        <div className={`font-semibold ${text}`}>{task.status}</div>
-        <div className="italic">{dayjs(task.deadline).format("DD-MM-YY")}</div>
+      <div className="flex justify-between pb-2 text-xs">
+        <div className={twMerge("rounded-lg px-2", statusStyle)}>{task.status}</div>
+        <div>{dayjs(task.deadline).format("DD-MM-YY")}</div>
       </div>
 
-      <div className="font-semibold">{task.description}</div>
+      <div className="font-medium">{task.description}</div>
     </Link>
   );
 };
