9f1fff12358f16ea7351a5ce3a263cdb85b2b5b5
mstjepan28 <markovcics@gmail.com>
2024-12-08 20:40:44 +0100

    create and update task

--- src/actions/taskActions.ts
+++ src/actions/taskActions.ts
@@ -1,8 +1,31 @@
 "use server";
 
+import { TaskStatus } from "~/enums/taskStatus";
+import { taskFormSchema } from "~/schema/taskSchemas";
+import { api } from "~/trpc/server";
 import type { NewTask } from "~/types/task";
 
-export const submitTaskAction = async (prevState: NewTask | null, formData: FormData) => {
-  console.log("Task submitted", prevState, formData);
-  return null;
+export const submitTaskAction = async (originalTaskId: string | null, formData: FormData) => {
+  console.log(formData);
+
+  const taskData: NewTask = taskFormSchema.parse({
+    assignedTo: formData.get("assignedTo"),
+    deadline: new Date(formData.get("deadline") as string),
+    description: formData.get("description"),
+    points: Number(formData.get("points")),
+    repeatCycle: formData.get("repeatCycle"),
+    status: formData.get("status") ?? TaskStatus.PENDING,
+  });
+
+  if (originalTaskId) {
+    await api.task.updateTask({
+      id: originalTaskId,
+      data: taskData,
+    });
+
+    return originalTaskId;
+  } else {
+    const newTask = await api.task.createTask(taskData);
+    return newTask.id;
+  }
 };
--- src/server/api/routers/taskRouter.ts
+++ src/server/api/routers/taskRouter.ts
@@ -1,9 +1,5 @@
-import { auth } from "~/server/auth";
-import dayjs from "dayjs";
-import { create } from "domain";
 import { z } from "zod";
-import { RepeatCycle } from "~/enums/repeatCycle";
-import { TaskStatus } from "~/enums/taskStatus";
+import { taskFormSchema } from "~/schema/taskSchemas";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const taskRouter = createTRPCRouter({
@@ -36,38 +32,39 @@ export const taskRouter = createTRPCRouter({
   /*************************************************/
   /***************** CREATE ROUTES *****************/
   /*************************************************/
-  createTask: protectedProcedure
+  createTask: protectedProcedure.input(taskFormSchema).query(async ({ input, ctx }) => {
+    if (!ctx.session.user.id) {
+      throw new Error("User not authenticated");
+    }
+
+    const task = await ctx.db.task.create({
+      data: {
+        description: input.description,
+        points: input.points,
+        repeatCycle: input.repeatCycle,
+        status: input.status,
+        deadline: input.deadline,
+        assignedTo: input.assignedTo,
+        assignedBy: ctx.session.user.id,
+      },
+    });
+
+    return task;
+  }),
+  updateTask: protectedProcedure
     .input(
       z.object({
-        description: z.string().max(255),
-        points: z.number().int().positive().max(100).min(1),
-        repeatCycle: z.nativeEnum(RepeatCycle),
-        status: z.nativeEnum(TaskStatus),
-        deadline: z.coerce.date().transform((d) => dayjs(d).toISOString()),
-        assignedTo: z.string().uuid(),
+        id: z.string().uuid(),
+        data: taskFormSchema.partial(),
       }),
     )
-    .query(async ({ input, ctx }) => {
-      if (!ctx.session.user.id) {
-        throw new Error("User not authenticated");
-      }
-
-      const task = await ctx.db.task.create({
-        data: {
-          description: input.description,
-          points: input.points,
-          repeatCycle: input.repeatCycle,
-          status: input.status,
-          deadline: input.deadline,
-          assignedTo: input.assignedTo,
-          assignedBy: ctx.session.user.id,
-          createdAt: dayjs().toISOString(),
-          updatedAt: dayjs().toISOString(),
-          deletedAt: null,
-        },
+    .mutation(async ({ input, ctx }) => {
+      const updatedTask = await ctx.db.task.update({
+        where: { id: input.id },
+        data: input.data,
       });
 
-      return task;
+      return updatedTask;
     }),
 
   /*************************************************/
